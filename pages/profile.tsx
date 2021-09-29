fa176f6890855f05362a10718ca1c97e62226375
ian038 <ianphua3@gmail.com>
2021-09-29 19:26:24 -0400

    WIP signin form

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -4,7 +4,22 @@ import { CognitoUser } from '@aws-amplify/auth';
 import { CognitoHostedUIIdentityProvider } from '@aws-amplify/auth/lib/types';
 import '../amplifyConfigure'
 
+type formState = {
+    email: String,
+    password: String,
+    authCode: String
+}
+
+const initialState: formState = {
+    email: '',
+    password: '',
+    authCode: ''
+}
+
 const Profile: React.FC<{}> = () => {
+    const [uiState, setUiState] = useState<String | null>(null)
+    const [formState, setFormState] = useState<formState>(initialState)
+
     useEffect(() => {
         checkUser()
         async function checkUser() {
@@ -13,17 +28,27 @@ const Profile: React.FC<{}> = () => {
         }
     }, [])
 
+    const onChange = e => {
+        setFormState({ ...formState, [e.target.name]: e.target.value })
+    }
+
     return (
         <div>
-            <button onClick={() => Auth.federatedSignIn({ provider: CognitoHostedUIIdentityProvider.Google })}>
-                Sign In with Google
-            </button>
-            <button onClick={() => Auth.federatedSignIn({ provider: CognitoHostedUIIdentityProvider.Facebook })}>
-                Sign In with Facebook
-            </button>
-            <button onClick={() => Auth.signOut()}>
-                Sign Out
-            </button>
+            {
+                uiState === 'signIn' && (
+                    <div>
+                        <button onClick={() => Auth.federatedSignIn({ provider: CognitoHostedUIIdentityProvider.Google })}>
+                            Sign In with Google
+                        </button>
+                        <button onClick={() => Auth.federatedSignIn({ provider: CognitoHostedUIIdentityProvider.Facebook })}>
+                            Sign In with Facebook
+                        </button>
+                        <button onClick={() => Auth.signOut()}>
+                            Sign Out
+                        </button>
+                    </div>
+                )
+            }
         </div>
     )
 }
