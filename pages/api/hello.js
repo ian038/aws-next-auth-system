d2e21d89a63612c888636b5eef69e327ff5e7045
ian038 <ianphua3@gmail.com>
2021-10-07 16:06:33 -0400

    Added amplify auth and completed app

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,12 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { withSSRContext } from 'aws-amplify'
+import '../../amplifyConfigure'
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  const { Auth } = withSSRContext({ req })
+  try {
+    const user = await Auth.currentAuthenticatedUser()
+    res.status(200).json({ name: user })
+  } catch(error) {
+    res.status(400).json({ error })
+  }
 }
