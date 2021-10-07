cf84a06eb8dfaf12a8ea62fa83753eb86c07c67f
ian038 <ianphua3@gmail.com>
2021-10-07 16:20:58 -0400

    Configured for deployment

--- aws-exports.js
+++ aws-exports.js
@@ -16,8 +16,8 @@ const awsmobile = {
             "profile",
             "aws.cognito.signin.user.admin"
         ],
-        "redirectSignIn": "http://localhost:3000/",
-        "redirectSignOut": "http://localhost:3000/",
+        "redirectSignIn": "https://aws-next-auth-system.vercel.app/",
+        "redirectSignOut": "https://aws-next-auth-system.vercel.app/",
         "responseType": "code"
     },
     "federationTarget": "COGNITO_USER_POOLS",
